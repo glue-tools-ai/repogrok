a219c9e7c98d27a926062c102f53876c362b19d0
Vaibhav Verma <2973492+verma6uc@users.noreply.github.com>
2025-12-08 11:30:00 +0530

    Wire all output formats into generator

--- src/output/generator.js
+++ src/output/generator.js
@@ -1,4 +1,7 @@
 import { generateXml } from './xmlStyle.js';
+import { generateMarkdown } from './markdownStyle.js';
+import { generateJson } from './jsonStyle.js';
+import { generatePlain } from './plainStyle.js';
 import { getPromptTemplate } from '../prompts/templates.js';
 
 export function generateOutput({ rootDir, files, directoryTree, gitDiff, gitLog, config }) {
@@ -18,6 +21,9 @@ export function generateOutput({ rootDir, files, directoryTree, gitDiff, gitLog,
 
   switch (config.output.style) {
     case 'xml': return generateXml(data);
+    case 'markdown': return generateMarkdown(data);
+    case 'json': return generateJson(data);
+    case 'plain': return generatePlain(data);
     default: return generateXml(data);
   }
 }
