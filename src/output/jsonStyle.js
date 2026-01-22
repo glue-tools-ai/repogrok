4286107265d885f277b11e19f84ad72b7f57060f
Nate Thompson <11262741+thompsonate@users.noreply.github.com>
2026-01-22 13:30:00 +0530

    Add analysis sections to all output formatters

--- src/output/jsonStyle.js
+++ src/output/jsonStyle.js
@@ -1,4 +1,4 @@
-export function generateJson({ rootDir, files, directoryTree, gitDiff, gitLog, instruction, totalFiles, totalCharacters }) {
+export function generateJson({ rootDir, files, directoryTree, gitDiff, gitLog, instruction, totalFiles, totalCharacters, languageStats, dependencyGraph, importanceRanking }) {
   const data = {
     summary: {
       repository: rootDir,
@@ -10,6 +10,18 @@ export function generateJson({ rootDir, files, directoryTree, gitDiff, gitLog, i
     directoryStructure: directoryTree,
     files: Object.fromEntries(files.map(f => [f.path, f.content])),
   };
+
+  if (languageStats && languageStats.length > 0) {
+    data.languageStatistics = languageStats;
+  }
+
+  if (dependencyGraph) {
+    data.dependencyGraph = dependencyGraph;
+  }
+
+  if (importanceRanking && importanceRanking.length > 0) {
+    data.importanceRanking = importanceRanking;
+  }
 
   if (gitDiff) data.gitDiffs = gitDiff;
   if (gitLog) data.gitLogs = gitLog;
