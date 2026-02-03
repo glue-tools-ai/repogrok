9c9439554f8941266e163001e56facccb5f1eb50
Alberto Rivera <31854351+aRiveraMerida@users.noreply.github.com>
2026-02-03 13:00:00 +0530

    Update public API exports for v1.1 modules

--- src/index.js
+++ src/index.js
@@ -3,3 +3,7 @@ export { searchFiles } from './core/fileSearch.js';
 export { collectFiles } from './core/fileCollect.js';
 export { generateOutput } from './output/generator.js';
 export { countTokens, estimateCost } from './core/tokenCount.js';
+export { optimizeForBudget, parseBudget } from './core/budgetOptimizer.js';
+export { calculateLanguageStats, formatLanguageStats } from './core/languageStats.js';
+export { rankFilesByImportance, formatImportanceRanking } from './core/importanceRanker.js';
+export { buildDependencyGraph, formatDependencyGraph } from './core/dependencyGraph.js';
